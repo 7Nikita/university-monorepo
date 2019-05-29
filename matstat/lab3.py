7cbcbac25f10b660577a9d2ef210efb8eb174ebd
Nikita <ram_page@icloud.com>
2019-05-29 19:39:47 +0300

    fixed lab3

--- matstat/lab3.py
+++ matstat/lab3.py
@@ -9,10 +9,16 @@ dev = 2 * math.sqrt(3)
 #######################
 
 
+def G(y):
+    val = 0
+    for elem in Y:
+        val += np.heaviside(y - elem, 1)
+    return val / n
+
+
 a, b = find_interval(exp, dev)
 X = []
 Y = []
-
 
 X, Y, a, b = gen(a, b, n)
 
@@ -43,6 +49,7 @@ print(f'chi2 = {chi2_test}, hi_table (for alpha = {alpha}, k = {M - 1}) = {chi2_
 print('accept' if chi2_test < chi2_critical else 'decline')
 
 n = 30
+a, b = find_interval(exp, dev)
 X, Y, a, b = gen(a, b, n)
 
 d = max(abs(F(Y[i]) - empirical_func(Y[i], Y, n)) for i in range(n))
@@ -53,11 +60,14 @@ print('#' * 100)
 print(f'd = {d}, lambda = {l}, critical lambda = {critical_l}')
 print('accept' if l < critical_l else 'decline')
 
-n = 50
+n = 5
+a, b = find_interval(exp, dev)
 X, Y, a, b = gen(a, b, n)
+Y = [3.34, 14.637, 20.539, 22.087, 25.028]
 
 omega2_critical = 0.744
-omega2 = sum((F(Y[i]) - empirical_func(Y[i], Y, n)) ** 2 for i in range(n)) + 1 / (12 * n)
+omega2 = sum((F(Y[i]) - (i + 0.5) / n) ** 2 for i in range(n)) + 1 / (12 * n)
+#omega2 = sum((F(Y[i]) - empirical_func(Y[i], Y, n)) ** 2 for i in range(n)) + 1 / (12 * n)
 print('#' * 100)
 print(f'omega2 = {omega2}, critical omega2 = {omega2_critical}')
 print('accept' if omega2 < omega2_critical else 'decline')
